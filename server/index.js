291403adccbbe1a1265ba6f428f204628183b6ab
Vip-Bhardw <vipulbhardwaj1011@gmail.com>
2021-05-27 21:39:17 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('../wumpus-world/node_modules/express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
